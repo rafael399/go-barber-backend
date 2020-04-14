16ed1643bbb627964b8398940d6c722f43c320ad
rafael399 <rafael399@gmail.com>
2020-04-14 16:37:13 -0300

    Relacionamento nos models

--- src/database/migrations/1586883769786-CreateAppointments.ts
+++ src/database/migrations/1586883769786-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1586883769786
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
